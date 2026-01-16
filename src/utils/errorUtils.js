d081d3da95d6155217e0a8944cd0f453662e3a7b
Witold Pacholik <witold778899@gmail.com>
2026-01-16 23:11:22 +0100

    adaptation to new versions of error response

--- src/utils/errorUtils.js
+++ src/utils/errorUtils.js
@@ -2,11 +2,14 @@ export const extractErrorMessage = (error) => {
   if (typeof error === 'string')
     return error;
 
-  if (error?.exceptions?.length > 0)
-    return error.exceptions[0].message;
+  if (error?.userMessage)
+    return error.userMessage;
+
+  if (error?.details?.length > 0)
+    return error.details[0].message;
 
   if (error?.message)
     return error.message;
 
-  return 'Something went wrong. Please try again.';
+  return 'Something went wrong. Please try again';
 };
